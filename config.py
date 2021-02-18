ce5f9eac9fc86e6e41a609a4fc3a0a5e401627c0
mrsohailkhanbot <60843252+mrsohailkhanbot@users.noreply.github.com>
2021-02-18 13:33:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+from sample_config import Config
+
+
+class Development(Config):
+  #get this values from the my.telegram.org
+  APP_ID = 1089218
+  API_HASH = "b6bdbb931516e5f9405f96ae129a412a"
+  # the name to display in your alive message
+  ALIVE_NAME = "ꜱōʜᴇʀᴜ ᴋāɴ"
+  # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+  DB_URI = "postgres://oaloxrsl:ND3k_nH8HxlfhmOeGDvLmr1uN_1IObeT@ziggy.db.elephantsql.com:5432/oaloxrsl"
+  #After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+  STRING_SESSION = "1BVtsOHkBu1J6BAVehZFxXDe80SPKVDOl81RKefB9QuwDyHH9SJvud3GxyBkb2nk7TYom-kEv9ETVrLNkbL4rnqBiUu_9Ji6bcj3G7siBsT5RWocen5en9vHP7yDJ8Pf0I2q_ihNlWKyX2VezTLlCkq4BIkyhCZdgiUmvlsOaq7gtPEpOPVfdnuprmyrT0TCxQWxUKxQAq4KlPYBPzbxtuHGgfMupk8fHxrw2bgREixfg_1auqeVcUeLB5RgWzu25i5JXhJHFLyNwjJbHBq9IMs-QPFQPzEmzzEnjXa7W4fp1rBkKZTuzxRA1UDzegNZflERR-WIF6025gO65iMKBluO_tcuoZYw="
+  #create a new bot in @botfather and fill the following vales with bottoken and username respectively
+  TG_BOT_TOKEN_BF_HER = "1265446625:AAER6jSzNDdHE4kRv66ko_qY_RDmVlVSxus"
+  TG_BOT_USER_NAME_BF_HER = "@khanteam_userbot"
+  #create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+  PRIVATE_GROUP_BOT_API_ID = -492979558
+  #command handler 
+  COMMAND_HAND_LER = "."
+  #sudo enter the id of sudo users userid's in that array
+  SUDO_USERS = [-1165790557]
+  # command hanler for sudo
+  SUDO_COMMAND_HAND_LER = "."
+  # GOOGLE DRIVE VARS SETUP HERE
+  G_DRIVE_CLIENT_ID = "105053195531-k6lil4953uvlcfap9pbgdm9rio51ccl4.apps.googleusercontent.com"
+  G_DRIVE_CLIENT_SECRET = "v0eF2RcSFRUuoCzzPWlPRjWa"
+  # SCREENSHOTLAYER ACCESS KEY
+  SCREEN_SHOT_LAYER_ACCESS_KEY = "3c67df6610916666939a5a6955c4860f" 
+  # WEATER API KEYS
+  OPEN_WEATHER_MAP_APPID = "a1773815e746c85ce85e744eb7667a8d"
+  #TELEGRAPH NAME
+  TELEGRAPH_SHORT_NAME = "ꜱōʜᴇʀᴜ ᴋāɴ"
